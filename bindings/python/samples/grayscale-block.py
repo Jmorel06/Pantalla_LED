bc2bd0190fb35797750cdb506f78a73da96fea6e
Jmorel06 <96885663+Jmorel06@users.noreply.github.com>
2021-12-30 14:07:54 -0500

    Update grayscale-block.py

--- bindings/python/samples/grayscale-block.py
+++ bindings/python/samples/grayscale-block.py
@@ -19,14 +19,14 @@ class GrayscaleBlock(SampleBase):
             for y in range(0, height):
                 for x in range(0, width):
                     c = sub_blocks * int(y / y_step) + int(x / x_step)
-                    if count % 4 == 0:
-                        self.matrix.SetPixel(x, y, c, c, c)
-                    elif count % 4 == 1:
-                        self.matrix.SetPixel(x, y, c, 0, 0)
-                    elif count % 4 == 2:
-                        self.matrix.SetPixel(x, y, 0, c, 0)
-                    elif count % 4 == 3:
-                        self.matrix.SetPixel(x, y, 0, 0, c)
+                    #if count % 4 == 0:
+                    self.matrix.SetPixel(x, y, c, c, c)
+                    #elif count % 4 == 1:
+                     #   self.matrix.SetPixel(x, y, c, 0, 0)
+                    #elif count % 4 == 2:
+                     #   self.matrix.SetPixel(x, y, 0, c, 0)
+                    #elif count % 4 == 3:
+                     #   self.matrix.SetPixel(x, y, 0, 0, c)
 
             count += 1
             time.sleep(2)
