dc8fdf5e0af1d360780e9bb04a9931c45a18b80e
Jmorel06 <96885663+Jmorel06@users.noreply.github.com>
2022-01-07 16:34:51 -0500

    Update runtext.py

--- bindings/python/samples/runtext.py
+++ bindings/python/samples/runtext.py
@@ -8,6 +8,7 @@ import time
 class RunText(SampleBase):
     def __init__(self, *args, **kwargs):
         super(RunText, self).__init__(*args, **kwargs)
+        self.matrix.setTextSize(2)
         self.parser.add_argument("-t", "--text", help="The text to scroll on the RGB LED panel", default="Hello world!")
 
     def run(self):
